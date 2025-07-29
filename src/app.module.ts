09ac19fc928a3ae20d3b58ce09e130cb233ddf16
Dan <hapoza@icloud.com>
2025-07-29 20:23:37 -0300

    Remove AppController, AppService, and associated tests to streamline the project structure and eliminate unused boilerplace code.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [PostsModule, CommentsModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
